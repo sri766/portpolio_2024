c5eeec3925a3d7c1701eed277a037ffb632717de
sri766 <srisanthseth28@gmail.com>
2024-06-25 17:54:11 +0530

    added path

--- src/app/project/page.jsx
+++ src/app/project/page.jsx
@@ -15,7 +15,7 @@ const features = [
     description: "We automatically save your files as you type.",
     href: "/",
     cta: "Learn more",
-    background: <Image height={500} width={500} className="absolute -right-20 -top-20 opacity-60" src="/assets/portfolio.png" />,
+    background: <Image height={500} width={500} className="absolute -right-20 -top-20 opacity-60" src="/assets/project/portfolio.png" />,
     className: "lg:row-start-1 lg:row-end-4 lg:col-start-2 lg:col-end-3",
   },
   {
@@ -24,7 +24,7 @@ const features = [
     description: "Search through all your files in one place.",
     href: "/",
     cta: "Learn more",
-    background: <Image height={500} width={500} className="absolute -right-20 -top-20 opacity-60" src="/assets/movie1.png" />,
+    background: <Image height={500} width={500} className="absolute -right-20 -top-20 opacity-60" src="/assets/project/movie1.png" />,
     className: "lg:col-start-1 lg:col-end-2 lg:row-start-1 lg:row-end-3",
   },
   {
@@ -33,7 +33,7 @@ const features = [
     description: "Supports 100+ languages and counting.",
     href: "/",
     cta: "Learn more",
-    background: <Image height={500} width={500} className="absolute -right-20 -top-20 opacity-60" src="/assets/movie2.png" />,
+    background: <Image height={500} width={500} className="absolute -right-20 -top-20 opacity-60" src="/assets/project/movie2.png" />,
     className: "lg:col-start-1 lg:col-end-2 lg:row-start-3 lg:row-end-4",
   },
   {
@@ -42,7 +42,7 @@ const features = [
     description: "Use the calendar to filter your files by date.",
     href: "/",
     cta: "Learn more",
-    background: <Image height={500} width={500} className="absolute -right-20 -top-20 opacity-60" src="/assets/disney1.png" />,
+    background: <Image height={500} width={500} className="absolute -right-20 -top-20 opacity-60" src="/assets/project/disney1.png" />,
     className: "lg:col-start-3 lg:col-end-3 lg:row-start-1 lg:row-end-2",
   },
   {
@@ -52,7 +52,7 @@ const features = [
       "Get notified when someone shares a file or mentions you in a comment.",
     href: "/",
     cta: "Learn more",
-    background: <Image height={500} width={500} className="absolute -right-20 -top-20 opacity-60" src="/assets/indiquest1.png" />,
+    background: <Image height={500} width={500} className="absolute -right-20 -top-20 opacity-60" src="/assets/project/indiquest1.png" />,
     className: "lg:col-start-3 lg:col-end-3 lg:row-start-2 lg:row-end-4",
   },
 ];
