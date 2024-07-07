9ed344183fff42b51d77777f0ed758530e4b98c5
sri766 <srisanthseth28@gmail.com>
2024-07-08 02:12:00 +0530

    added version3

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ import Docker from './Docker'
 export default function Footer() {
   return (
     <div className="w-full flex flex-col justify-center items-center">
-        <div className="text-xl text-slate-500 flex justify-center items-center">
-            <span className="ml-2">
+        <div className="text-xl text-slate-500 flex flex-col justify-center items-center">
+            <span className="ml-2 flex-inline">
                 Made with 
                 <HeartIcon className="text-purple-500 h-6 w-6 inline m-2" />
                 by Srisanth
